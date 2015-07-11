c65b7fd86c356defdb1f6a935a9a85327f90c0b1
Thomas Reis <tmichaelreis@gmail.com>
2015-07-11 03:53:45 +0000

    Add title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  #Returns full app title if needed
+  def full_title(page_title = '')
+    base_title = "Book Club"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
