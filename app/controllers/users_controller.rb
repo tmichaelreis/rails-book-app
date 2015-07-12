43d63cc0b658c7f66925695d1b8e96410a64416a
Thomas Reis <tmichaelreis@gmail.com>
2015-07-12 03:49:36 +0000

    Add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,9 +14,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the book club!"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "Check your email to activate account."
+      redirect_to root_url
     else
       render 'new'
     end
